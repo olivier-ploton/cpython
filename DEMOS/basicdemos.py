fdbc7c3494ca49afed3fe8cfd758064564961b65
Olivier Ploton <olivier.ploton@univ-tours.fr>
2024-11-11 16:46:16 +0100

    Update demos

--- DEMOS/basicdemos.py
+++ DEMOS/basicdemos.py
@@ -2,32 +2,130 @@
 Some demos with 0-based and 1-based indicies, and closed slices.
 """
 
-import builtins
+def PRINT(*args, **kwargs):
+    hrule = '-' * 60
+    print()
+    print(hrule)
+    print(*args, **kwargs)
+    print(hrule)
+    print()
 
 ########################################
 
-class SLICER(object):
-    """
-    class of SLICE, used to convert an expression in brackets / braces
-    into a slice(...) or extension.
-    """
+PRINT("The a{i} notation work on every indexable object")
+
+# Outside of a Computer Science context, items are often numbered starting from 1
+a = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th"]
+
+print(f"{a = }")
+print()
+
+print(f"{a[0] = }")
+print(f"{a[1] = }")
+print(f"{a[2] = }")
+print()
+
+print(f"{a{1} = }")
+print(f"{a{2} = }")
+print(f"{a{3} = }")
+print()
+
+print(f"{a[-1] = }")
+print(f"{a[-2] = }")
+print(f"{a[-3] = }")
+print()
+
+print(f"{a{-0} = }")
+print(f"{a{-1} = }")
+print(f"{a{-2} = }")
+print()
+
+PRINT("Slices, semi-open and closed")
+
+print(f"{a[0::2] = }")
+print(f"{a[1::2] = }")
+print()
+
+print(f"{a{1::2} = }")
+print(f"{a{2::2} = }")
+print()
+
+print(f"{a[0:7:2] = }")
+print(f"{a{1:8:2} = }")
+print(f"{a{1:7::2}= }")
+print()
+
+print(f"{a[1:8:2] = }")
+print(f"{a{2:9:2} = }")
+print(f"{a{2:8::2}= }")
+print()
+
+print("From 4th to 8th element, inclusive")
+print(f"{a[3:8]   = }")
+print(f"{a{4:8::} = }")
+print()
+
+########################################
+
+PRINT("Some swapping")
+
+print(f"{a =}")
+
+print("Let us swap the 4-th and the 8-th element:")
+a[3], a[7] = a[7], a[3]
+print(f"{a =}")
+
+print("Let us swap the 4-th and the 8-th element, again:")
+a{4}, a{8} = a{8}, a{4}
+print(f"{a =}")
+print()
+
+print("Let us swap and reverse the 3 first and last elements:")
+for i in range(3):
+    a[i], a[-i-1] = a[-i-1], a[i]
+print(f"{a =}")
+
+print("Let us swap and reverse the 3 first and last elements, again:")
+for i in range(3):
+    a[i], a{-i} = a{-i}, a[i]
+print(f"{a =}")
+
+########################################
+
+PRINT("Mixing 0-based and 1-based indices")
+
+class Reveal:
     def __getitem__(self, arg):
         return arg
 
-SLICE = SLICER()
+reveal = Reveal()
+
+print("Indices for the 4th line and 8th column of a 2D array")
+print(f"{reveal[3, 7]  = }")
+print(f"{reveal{4, 8}  = }")
+print(f"{reveal[3].{8} = }")
+print(f"{reveal{4}.[7] = }")
 
 ########################################
 
-def test(*args):
+# This part shows details of the proof-of-concept implementation:
+# parse-time AST expansion of extended indices/slices into standard ones.
+# Use only out of curiosity or for debugging.
+
+def printexpansion(*args):
+    """
+    prints the AST expansion of fragments of code with extended indices
+    Each argument is a string containing a fragment of code
+    """
     import ast
     for code in args:
-        print(code, '->', end=' ')
+        print(code, '--->', end=' ')
         try:
             print(ast.unparse(ast.parse(code)))
         except Exception as e:
             print(e.__class__.__name__)
 
-def tests():
+def testexpansions():
     brackets = [
         "a[i]",
         
@@ -53,56 +151,14 @@ def tests():
         "a[:j::]",
         "a[:::]", # closed a[:], no effect by default
     ]
-    test(*brackets)
+    printexpansion(*brackets)
     print()
     braces = [code.replace("[", "{").replace("]", "}") for code in brackets]
-    test(*braces)
+    printexpansion(*braces)
+    print()
+    printexpansion("a{i, *s, j}", "a[i].{j}")
 
-########################################
-
-class _seq(object):
-    def iterable(self, arg):
-        """
-        transforms its argument into an iterable
-        """
-        if isinstance(arg, slice):
-            # non-integer values not taken into account
-            start = 0 if arg.start is None else arg.start
-            step = 1 if arg.step is None else arg.step
-            if arg.stop is not None:
-                return range(start, arg.stop, step)
-            else:
-                # unlimited iterations
-                import itertools
-                return itertools.count(start, step)
-        else:
-            # 1 iteration: arg itself
-            return (arg,)
-    
-    def __xkey__(self, arg, braces, closed):
-        if braces:
-            raise IndexError("seq{...} is forbidden, use seq[...]")
-        if closed:
-            # For stop we can never transform an int into None,
-            # because in this context, None means unlimited
-            assert isinstance(arg, slice)
-            start = arg.start
-            stop = arg.stop
-            step = 1 if arg.step is None else arg.step
-            if isinstance(stop, int) and isinstance(step, int):
-                stop += (step > 0) - (step < 0)
-            return slice(start, stop, arg.step)
-    
-    def __getitem__(self, key):
-        args = key if isinstance(key, tuple) else (key,)
-        import itertools
-        return itertools.chain(*(self.iterable(arg) for arg in args))
-        
-seq = _seq()
-
-# problème pour 10:0::-1 car ça se transforme en 10:None:-1 = 10::-1
-# normalement résolu avec le nouveau seq
-# for i in seq[1:3::, -18, 5, 10:0::-1]: print(i)
-
+PRINT("Implemetation detail: AST expansion of extended indices")
+testexpansions()
 
 ########################################
--- DEMOS/closedslices.py
+++ DEMOS/closedslices.py
@@ -3,71 +3,156 @@ Using semi-open / closed slices to explicitly state
 whether the upper bound is excluded or included
 """
 
+def PRINT(*args, **kwargs):
+    hrule = '-' * 60
+    print()
+    print(hrule)
+    print(*args, **kwargs)
+    print(hrule)
+    print()
 
-def subscriptable(f):
+########################################
+
+PRINT("Using range_[i:j::k] to explicitly include upper bound")
+
+class _range_:
     """
-    @subscriptable
-    def f(*args):
-        ...
-    
-    Transforms the function f into a subscritable, non-functional object:
-    we can write f[*args], and each argument can be a slice (but no kwargs).
-    Because functions themselves will perhaps be subscriptable by types
-    in the future (PEP 718 ?), we avoid making the resulting object callable.
-    
-    This should be typed, but how ?
+    A use case for closed slices: transform a slice into a finite iterable
+    Well-known idea (folklore), countless messages about it on the net.
+    range[i:j:k] essentially abbreviates range(i, j, k).
+    range[i:j::k] is very useful to express that j must be included
     """
-    class C(object):
-        def __getitem__(self, args):
-            if isinstance(args, tuple):
-                return f(*args)
+    def __getitem__(self, key):
+        if isinstance(key, slice):
+            step = 1 if key.step is None else key.step
+            start = 0 if key.start is None else key.start                
+            stop = (step > 0) - (step < 0) if key.stop is None else key.stop
+            return range(start, stop, step)
+        elif isinstance(key, tuple):
+            raise ValueError("range_[i1, i2, ...] is forbidden")
+        else:
+            raise ValueError("range_[n] is forbidden, use range(n) instead")
+        
+        import itertools
+        return itertools.chain(*(self.iterable(arg) for arg in args))
+
+range_ = _range_()
+
+n = 10
+print(f"{sum(1/i**2 for i in range(1, n+1)) = }")
+print(f"{sum(1/i**2 for i in range_[1:n::]) = }")
+print()
+
+print(f"{[i for i in range_[:n]] = }")
+print(f"{[i for i in range_[:n::]] =}")
+print(f"{[i for i in range_[1:n::]] = }")
+print()
+
+print(f"{[i for i in range_{n::-1}]        = }")
+print(f"{[i for i in reversed(range_[:n])] = }")
+print()
+
+print(f"{[i for i in range_{n:1:-1}]        = }")
+print(f"{[i for i in reversed(range_[1:n])] = }")
+print()
+
+print(f"{[i for i in range_[-n:]] = }")
+print(f"{[i for i in range_[-n:0]] = }")
+
+########################################
+
+PRINT("seq: multiple ranges chained (closed slices, custom __xkey__)")
+
+class _seq:
+    """
+    A use case for closed slices, and for custom __xkey__:
+    transform a list of slices into a potentially infinite iterable.
+    
+    Without a custom __xkey__, seq[n:0::-1] is transformed in a context of
+    finite lists where :0:: means "beginning included" and becomes seq[n::-1].
+    But this means seq[n:-oo:-1] in the context of inifinite iterations.
+    A custom __xkey__ solves this issue.
+    """
+    def iterable(self, arg):
+        """
+        transforms its argument into an iterable
+        """
+        if isinstance(arg, slice):
+            # non-integer values not taken into account
+            start = 0 if arg.start is None else arg.start
+            step = 1 if arg.step is None else arg.step
+            if arg.stop is not None:
+                return range(start, arg.stop, step)
             else:
-                return f(args)
-    return C()
-
-@subscriptable
-def slices(*args):
-    return args
-
-@subscriptable
-def range_(arg):
-    """
-    The classical range function.
-    Many variations on the net (Python Discourse, StackExchange, ...)
-    """
-    if isinstance(arg, slice):
-        start = 0 if arg.start is None else arg.start
-        step = 1 if arg.step is None else arg.step
-        if arg.stop is not None:
-            return range(start, arg.stop, step)
+                # unlimited iterations
+                import itertools
+                return itertools.count(start, step)
         else:
-            import itertools
-            return itertools.count(start, step)
-    else:
-        raise ValueError("only range_[slice] allowed")
+            # 1 iteration: arg itself
+            return (arg,)
+    
+    def __xkey__(self, arg, braces, closed):
+        if braces:
+            raise ValueError("seq{...} is forbidden, use seq[...]")
+        if closed:
+            # For stop we can never transform an int into None,
+            # because in this context, None means unlimited
+            assert isinstance(arg, slice)
+            start = arg.start
+            stop = arg.stop
+            step = 1 if arg.step is None else arg.step
+            if isinstance(stop, int) and isinstance(step, int):
+                stop += (step > 0) - (step < 0)
+            return slice(start, stop, arg.step)
+    
+    def __getitem__(self, key):
+        args = key if isinstance(key, tuple) else (key,)
+        import itertools
+        return itertools.chain(*(self.iterable(arg) for arg in args))
+        
+seq = _seq()
 
-def iterable(arg):
+n = 10
+print(f"{sum(1/i**2 for i in range(1, n+1)) = }")
+print(f"{sum(1/i**2 for i in    seq[1:n::]) = }")
+print()
+
+# With seq[...] we have to manage a potentially infinite iterable
+def limited(iterable, maxlength=20):
     """
-    transforms its argument into an iterable
+    convert a potentially infinite iterable into a list with
+    at most maxlength elements (and an extra Ellipsis)
     """
-    if isinstance(arg, slice):
-        # non-integer values not taken into account
-        start = 0 if arg.start is None else arg.start
-        step = 1 if arg.step is None else arg.step
-        if arg.stop is not None:
-            return range(start, arg.stop, step)
-        else:
-            import itertools
-            return itertools.count(start, step)
-    else:
-        return (arg,)
+    result = []
+    l = 0
+    for i in iterable:
+        result.append(i)
+        l += 1
+        if l >= maxlength:
+            result.append(...)
+            break
+    return result
 
+print(f"{limited(seq[1:3::]) = }")
+print(f"{limited(seq[1:3::, 100, 3:1::-1]) = }")
+print(f"{limited(seq[5::10]) = }")
 
-@subscriptable
-def seq(*args):
-    print(args)
-    import itertools
-    return itertools.chain(*(iterable(arg) for arg in args))
+########################################
 
-# problème pour 10:0::-1 car ça se transforme en 10:None:-1 = 10::-1
-for i in seq[1:3::, -18, 5, 10:1::-1]: print(i)
+PRINT("Math: inverse of a permutation of {1, 2, ..., n}")
+
+# A permtation: each value between 1 and n inclusive appears exactly once
+p = [1, 5, 3, 2, 4]
+print(f"{p = }")
+
+# Let us compute q = p⁻¹
+q = [None] * len(p)
+for i in seq[1:len(p)::]: q{p{i}} = i
+print(f"{q = }")
+
+# Same computation wit 0-based indices
+q = [None] * len(p)
+for i in range(len(p)): q[p[i]-1] = i+1
+print(f"{q = }")
+
+########################################
